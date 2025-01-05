73dc05c34f4483795f7f92c3d1f9c62ced2bba57
Hector Figueroa <hfigueroa37@gmail.com>
2025-01-05 13:57:49 -0800

    modified unauthorized error message

--- src/pages/api/[id].ts
+++ src/pages/api/[id].ts
@@ -11,7 +11,13 @@ export const GET: APIRoute = ({ params, request }) => {
   const apiKey = request.headers.get('Authorization');
 
   if (!apiKey || !setAPIkey.includes(apiKey)) {
-    return new Response('Unauthorized', { status: 401 });
+    return new Response(
+      JSON.stringify({ error: "Unauthorized" }), 
+      { 
+        status: 401,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
   }
 
   const userAgent = request.headers.get('User-Agent') || '';
