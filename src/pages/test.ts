11a01c7a25a1edf07bfef8853f3979e0c0fd24a1
Hector Figueroa <hfigueroa37@gmail.com>
2025-01-06 00:18:59 -0800

    testig api route test 7

--- src/pages/test.ts
+++ src/pages/test.ts
@@ -1,17 +1,18 @@
 export const prerender = false;
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async () => {
-  const secret = process.env.API_SECRET || import.meta.env.API_KEY;
+export const GET: APIRoute = ({ request }) => {
 
-  if (!secret) {
+  const apiKey = request.headers.get('Authorization');
+
+  if (!apiKey) {
     return new Response("API_SECRET is not set or undefined.", {
       status: 500,
       headers: { 'Content-Type': 'text/plain' },
     });
   }
 
-  return new Response(`The value of API_SECRET is: ${secret}`, {
+  return new Response(`The value of API_SECRET is: ${apiKey}`, {
     status: 200,
     headers: { 'Content-Type': 'text/plain' },
   });
