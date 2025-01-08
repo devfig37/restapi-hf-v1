08c58b98a4dad6aee4cac5df073827c1ffaa2b28
Hector Figueroa <hfigueroa37@gmail.com>
2025-01-07 21:26:22 -0800

    commented out client side api key check. added unauthorized error

--- src/pages/test.ts
+++ src/pages/test.ts
@@ -1,21 +1,13 @@
 export const prerender = false;
 import type { APIRoute } from 'astro';
 import { API_SECRET } from "astro:env/server";
-import { API_KEY } from "astro:env/client";
+// import { API_KEY } from "astro:env/client";
 
 export const GET: APIRoute = ({ request }) => {
 
   const apiKey = request.headers.get('Authorization');
   var confirmServer = '';
-  var confirmClient = '';
-
-  if(apiKey === API_SECRET) {
-    confirmServer = 'Server API key working!';
-  };
-
-  if(apiKey === API_KEY) {
-    confirmServer = 'Client API key working!';
-  };
+  // var confirmClient = '';
 
   if (!apiKey) {
     return new Response("API_SECRET is not set or undefined.", {
@@ -24,7 +16,22 @@ export const GET: APIRoute = ({ request }) => {
     });
   }
 
-  return new Response(`${confirmServer || confirmClient}`, {
+  if(apiKey === API_SECRET) {
+    confirmServer = 'Server API key working!';
+  };
+
+  // if(apiKey === API_KEY) {
+  //   confirmServer = 'Client API key working!';
+  // };
+
+  if (apiKey !== API_SECRET) {
+    return new Response("Unauthorized", {
+      status: 401,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+
+  return new Response(`${confirmServer}`, {
     status: 200,
     headers: { 'Content-Type': 'text/plain' },
   });
