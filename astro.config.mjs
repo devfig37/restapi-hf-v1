3fcb59851f2a26da940270bfc206002d51683e9a
Hector Figueroa <hfigueroa37@gmail.com>
2025-01-07 21:18:53 -0800

    added server and client env config to astro config and set them in test route

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+    env: {
+      schema: {
+        API_SECRET: envField.string({
+          context: 'server',
+          access: 'secret',
+        }),
+        API_KEY: envField.string({ 
+          context: "client", 
+          access: "public", 
+          optional: true 
+        }),
+      }
+    },
 });
--- src/pages/test.ts
+++ src/pages/test.ts
@@ -1,9 +1,21 @@
 export const prerender = false;
 import type { APIRoute } from 'astro';
+import { API_SECRET } from "astro:env/server";
+import { API_KEY } from "astro:env/client";
 
 export const GET: APIRoute = ({ request }) => {
 
   const apiKey = request.headers.get('Authorization');
+  var confirmServer = '';
+  var confirmClient = '';
+
+  if(apiKey === API_SECRET) {
+    confirmServer = 'Server API key working!';
+  };
+
+  if(apiKey === API_KEY) {
+    confirmServer = 'Client API key working!';
+  };
 
   if (!apiKey) {
     return new Response("API_SECRET is not set or undefined.", {
@@ -12,7 +24,7 @@ export const GET: APIRoute = ({ request }) => {
     });
   }
 
-  return new Response(`The value of API_SECRET is: ${apiKey}`, {
+  return new Response(`${confirmServer || confirmClient}`, {
     status: 200,
     headers: { 'Content-Type': 'text/plain' },
   });
